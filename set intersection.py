c5d4b607f0de9be3bd653b94b58086e908616ac7
nayshakabrabro-lab <nayshakabrabro@gmail.com>
2026-06-14 16:33:45 -0700

    initial commit

---
+++ set intersection.py
@@ -0,0 +1,10 @@
+setx={"green","blue"}
+sety={"blue","yellow"}
+print("original set elements:")
+print(setx)
+print(sety)
+print("\ninternational of two said sets")
+setz=setx.intersection(sety)
+print(setz)
+setz=setx.union(sety)
+print(setz)
